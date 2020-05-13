32c68b67f646454d75b4bba9be87dea23864276a
zsw <2323493244@qq.com>
2020-05-13 13:58:43 +0800

    注册中心搭建

---
+++ forum-eureka7002/src/main/java/com/cfm/ForumEureka7002Application.java
@@ -0,0 +1,15 @@
+package com.cfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ForumEureka7002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumEureka7002Application.class, args);
+    }
+
+}
